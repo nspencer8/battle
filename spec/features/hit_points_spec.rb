301c5150db3fbebace19a4c6674f79909ac96918
naomi <naomi.emma.spencer@gmail.com>
2016-10-14 12:32:05 +0100

    Added testing for sign in and play

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,13 +2,7 @@ require "spec_helper"
 
 RSpec.feature "Battle", :type => :feature do
   scenario "show hit points" do
-    visit "/"
-
-    fill_in "player_1", :with => "Bob"
-    fill_in "player_2", :with => "Sue"
-
-    click_button "Enter"
-
+    sign_in_and_play
     expect(page).to have_text("Player 2 has 10 Hit Points")
   end
 end
--- spec/features/user_names_spec.rb
+++ spec/features/user_names_spec.rb
@@ -2,13 +2,7 @@ require "spec_helper"
 
 RSpec.feature "Battle", :type => :feature do
   scenario "Two users enter their names" do
-    visit "/"
-
-    fill_in "player_1", :with => "Bob"
-    fill_in "player_2", :with => "Sue"
-
-    click_button "Enter"
-
+    sign_in_and_play
     expect(page).to have_text("Welcome to Battle Bob and Sue.")
   end
 end
